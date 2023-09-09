61c63652e1dd759cfeeefacff8ed7e56a99a6f8a
Rasheed Alwahbany <rasheedalwahbany@gmail.com>
2023-09-10 01:01:30 +0300

    Initial the project with adding language controller and view

---
+++ FirstASPNETApplication/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstASPNETApplication.Models;
+
+namespace FirstASPNETApplication.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Language()
+        {
+            var language = new Language() { name="English"};
+
+            return View(language);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Hello "+ id);
+        }
+
+        public ActionResult Bydate(int year,int month)
+        {
+            return Content("Year="+year+" Month="+month);
+        }
+    }
+}
---
+++ FirstASPNETApplication/Models/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstASPNETApplication.Models
+{
+    public class Language
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
